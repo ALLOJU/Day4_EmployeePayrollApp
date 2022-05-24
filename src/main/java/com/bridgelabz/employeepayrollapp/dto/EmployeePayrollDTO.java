a93029f33bdaaaca1ab81299b14a8b5e108748a8
ALLOJU <mounika.alloju@gmail.com>
2022-05-24 17:03:32 +0530

    UC13 with remaining properties added

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,26 +1,27 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class EmployeePayrollDTO {
-    @Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Employee name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     @NotEmpty(message = "Employee Name can't be null")
     public String name;
+
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
-    public EmployeePayrollDTO(String name,long salary){
-        super();
-        this.name = name;
-        this.salary = salary;
-    }
+    public String gender;
 
-//    @Override
-//    public String toString() {
-//        return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
-//    }
+    @JsonFormat(pattern = "dd MMM yyyy")
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> departments;
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -7,31 +7,34 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
-@Entity
-@Table(name="employee")
+
 public class EmployeePayrollData {
-    @Id
-    @Column(name="id")
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 
     public EmployeePayrollData() {
-        // TODO Auto-generated constructor stub
-    }
-    public EmployeePayrollData(int employeeId, String name, long salary) {
-        super();
-        this.employeeId = employeeId;
-        this.name = name;
-        this.salary = salary;
-    }
-    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
 
-        super();
+    }
+
+    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.startDate = employeePayrollDTO.startDate;
+        this.note = employeePayrollDTO.note;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
     }
 
 }
